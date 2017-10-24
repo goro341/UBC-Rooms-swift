6a3d3fb0f9d8693eb7fb2c9494363ea58e6cf143
masoud76 <masoudjaveri@yahoo.com>
2017-10-24 10:40:26 -0700

    Initial Commit

---
+++ ICICS Rooms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ICICS Rooms
+//
+//  Created by Masoud on 2017-10-24.
+//  Copyright © 2017 Masoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
